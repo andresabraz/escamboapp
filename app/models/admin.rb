447a6c569900044e1d23364b358b53f6cad321f1
Andresa Braz <andresabraz@gmail.com>
2019-05-29 21:25:49 -0300

    Add some policies for admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,13 @@
 class Admin < ActiveRecord::Base
-  enum role: [:full_access, :restricted_access]
+  # Constants
+  ROLES = {:full_access => 0, :restricted_access => 1}
 
-  scope :with_full_access, -> { where(role: 'full_access') }
-  scope :with_restricted_access, -> { where(role: 'restricted_access') }
+  # Enums
+  enum role: ROLES
 
+  # Scopes
+  scope :with_full_access, -> { where(role: ROLES[:full_access]) }
+  scope :with_restricted_access, -> { where(role: ROLES[:restricted_access]) }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
